620253a708f0254bbad119fdde266d08646ec146
modjtaba22 <haidarzada@web.de>
2025-03-13 11:30:23 +0100

    Refactoring ,now it runs locally or docker with the same setup

--- app/db/session.py
+++ app/db/session.py
@@ -1,11 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.models.token import Base  
+import os
 
-# Database connection URL (using asyncpg driver)
-#DATABASE_URL = "postgresql+asyncpg://user:password@localhost/token_db"
-DATABASE_URL = "postgresql+asyncpg://user:password@db/token_db"
+# Check whether the environment is running in Docker or locally
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    # If no environment variable is set, check whether the application is running in Docker.
+    # Within Docker, the host name "db" is used, otherwise "localhost" for local development.
+    hostname = "db" if os.getenv("DOCKER_ENV") else "localhost"
+    DATABASE_URL = f"postgresql+asyncpg://user:password@{hostname}/token_db"
 
 # Create an asynchronous engine
 engine = create_async_engine(DATABASE_URL, echo=True)
